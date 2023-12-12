2dbae4d0320e041837db9321f4e16179ddb69d1e
lenguyenphuonglinh <phuonglinh260105@gmail.com>
2023-12-13 00:50:10 +0700

    Added description, humidity, wind, icon

--- index.js
+++ index.js
@@ -5,26 +5,42 @@ function namePrompt() {
 }
 window.addEventListener("load", namePrompt);
 
-function displayTemperature(response) {
+function displayWeather(response) {
   let temperatureElement1 = document.querySelector("#currentTemperature1");
   let temperatureElement2 = document.querySelector("#currentTemperature2");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector(".description");
+  let humidityElement = document.querySelector(".humidity");
+  let humidity = response.data.temperature.humidity;
+  let windElement = document.querySelector(".wind");
+  let wind = Math.round(response.data.wind.speed);
+  let iconElement = document.querySelector(".icon1");
 
   temperatureElement1.innerHTML = `It's currently ${temperature}°C`;
   temperatureElement2.innerHTML = `${temperature}°C`;
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${wind}km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon1" />`;
 }
 
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-text-input");
-  let city = searchInputElement.value;
+function searchCity(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=cc84c9820ab27141fa553331t4bbcao5&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayWeather);
 }
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+  searchCity(searchInput.value);
+}
+
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Hanoi");
 
 function updateDateTime(date) {
   let minutes = date.getMinutes();
@@ -48,6 +64,7 @@ function updateDateTime(date) {
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}`;
 }
+
 let currentDateELement = document.querySelector("#datetime");
 let currentDate = new Date();
 currentDateELement.innerHTML = updateDateTime(currentDate);
